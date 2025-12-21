1c5965e6eb15cfb1908c377906df9143e81ffe15
lalit7886 <lalitramanmishra@gmail.com>
2025-12-21 13:37:47 +0545

    setup adn pyproject.toml

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup ,find_packages
+setup(
+    name="src",
+    version="0.0.01",
+    author="Lalit Raman Mishra",
+    author_email="lalitramanmishra@gmail.com",
+    packages=find_packages()
+    
+)
